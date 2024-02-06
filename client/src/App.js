3943230ad0f0cb027ee44628871d664aa12ff00a
rishstack <2021.rishabh.gupta@ves.ac.in>
2024-02-07 03:32:39 +0530

    Frontend Created

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+/*Routes*/
+/*import all components*/
+import Username from './components/Username';
+import Password from './components/Password';
+import Reset from './components/Reset';
+import Recovery from './components/Recovery';
+import Profile from './components/Profile';
+import PageNotFound from './components/PageNotFound';
+import Register from './components/Register';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Username></Username>,
+  },
+  {
+    path: '/register',
+    element: <Register></Register>,
+  },
+  {
+    path: '/password',
+    element: <Password></Password>,
+  },
+  {
+    path: '/profile',
+    element: <Profile></Profile>,
+  },
+  {
+    path: '/recovery',
+    element: <Recovery></Recovery>,
+  },
+  {
+    path: '/reset',
+    element: <Reset></Reset>,
+  },
+  {
+    path: '*',
+    element: <PageNotFound></PageNotFound>,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
 
